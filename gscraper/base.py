82c685f7aa4fd20aa26f132f3312688b3405e4cd
minyeamer <minyeamer@gmail.com>
2023-08-19 21:14:44 +0900

    fix: condition for setting default kwargs on session

--- gscraper/base.py
+++ gscraper/base.py
@@ -197,7 +197,7 @@ class Spider(CustomDict):
     def requests_task(func):
         @functools.wraps(func)
         def wrapper(self: Spider, *args, **kwargs):
-            if not (args and kwargs): kwargs = self.__dict__.copy()
+            if not (args or kwargs): kwargs = self.__dict__.copy()
             results = func(self, *args, **kwargs)
             self.upload_data(results, **kwargs)
             return results
@@ -206,7 +206,7 @@ class Spider(CustomDict):
     def requests_session(func):
         @functools.wraps(func)
         def wrapper(self: Spider, *args, **kwargs):
-            if not (args and kwargs): kwargs = self.__dict__.copy()
+            if not (args or kwargs): kwargs = self.__dict__.copy()
             with requests.Session() as session:
                 results = func(self, *args, session=session, **kwargs)
             time.sleep(.25)
@@ -431,7 +431,7 @@ class AsyncSpider(Spider):
     def asyncio_task(func):
         @functools.wraps(func)
         async def wrapper(self: AsyncSpider, *args, **kwargs):
-            if not (args and kwargs): kwargs = self.__dict__.copy()
+            if not (args or kwargs): kwargs = self.__dict__.copy()
             semaphore = self.asyncio_semaphore(**kwargs)
             results = await func(self, *args, semaphore=semaphore, **kwargs)
             self.upload_data(results, **kwargs)
@@ -441,7 +441,7 @@ class AsyncSpider(Spider):
     def asyncio_session(func):
         @functools.wraps(func)
         async def wrapper(self: AsyncSpider, *args, **kwargs):
-            if not (args and kwargs): kwargs = self.__dict__.copy()
+            if not (args or kwargs): kwargs = self.__dict__.copy()
             semaphore = self.asyncio_semaphore(**kwargs)
             async with aiohttp.ClientSession() as session:
                 results = await func(self, *args, session=session, semaphore=semaphore, **kwargs)
