0ec346a0568b46f4e797c4cf927473102b0ec720
minyeamer <minyeamer@gmail.com>
2024-12-01 10:43:46 +0900

    fix: rename source distribution file for pypi upload

--- dist.sh
+++ dist.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 python -m setup.py sdist bdist_wheel
+for file in dist/gcp-scraper*; do
+    mv "$file" "${file/gcp-scraper/gcp_scraper}"
+done
 python -m twine upload dist/*
 rm -rf build
 rm -rf dist
