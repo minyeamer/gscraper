08de79af0350e8d102aa22dbc4662b1b630c0804
minyeamer <minyeamer@gmail.com>
2025-02-23 11:16:33 +0900

    update: requirements in setup

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ from setuptools import setup, find_packages
 from gscraper import __version__
 
 REQUIRES = [
-    "pandas>=1.1.4",
-    "openpyxl>=3.0.10",
     "tqdm>=4.64.0",
     "asyncio>=3.4.3",
+    "python-dateutil>=2.8.2",
+    "pandas>=1.4.2",
+    "openpyxl>=3.1.2",
     "bs4>=0.0.1",
-    "requests>=2.28.1",
-    "aiohttp>=3.8.3",
-    "aiohttp-proxy>=0.1.2",
-    "lxml>=4.9.1",
+    "requests>=2.31.1",
+    "aiohttp>=3.9.1",
+    "lxml>=4.9.4",
+    "PyJWT>=2.9.0",
     "gspread>=5.6.2",
     "google-cloud-bigquery>=3.4.0",
-    "pandas-gbq>=0.17.9"]
+    "holidays>=0.43",
+    "workalendar>=17.0.0"]
 
 setup(
     name="gcp-scraper",
